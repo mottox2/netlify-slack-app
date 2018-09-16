3646127285c5fab7c00456570601a86f09994f62
mottox2 <y.mottox2@gmail.com>
2018-09-16 17:57:00 +0900

    Get message

--- src/events.js
+++ src/events.js
@@ -1,6 +1,23 @@
+import { WebClient } from '@slack/client'
+
+// 書く
+// slack_tokenの取得
+// メッセージの取得方法
+
 exports.handler = (event, context, callback) => {
   const body = JSON.parse(event.body)
+  const event = body.event
   console.log(JSON.stringify(body, null, 4))
+
+  if (event && event.type === 'reaction_added' && event.item.type === 'message') {
+    const web = new WebClient(process.env.SLACK_TOKEN)
+    const result = await web.conversations.history({
+      latest: event.item.ts,
+      limit: 1,
+      channel: event.item.channel
+    })
+    console.log(result)
+  }
 
   callback(null, {
     statusCode: 200,
